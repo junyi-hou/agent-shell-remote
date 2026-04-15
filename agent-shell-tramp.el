8fdaf75b4ae96ef9385624b3ac7cfeb0958ac0bb
junyi.hou <junyi.yi.hou@gmail.com>
2026-04-14 21:38:47 -0700

    add copyright statements

--- agent-shell-tramp.el
+++ agent-shell-tramp.el
@@ -1,5 +1,26 @@
 ;;; agent-shell-tramp.el --- running agent-shell on remote machine using tramp -*- lexical-binding: t -*-
 
+;; Copyright (C) 2026 Junyi Hou
+
+;; Author: Junyi Hou <junyi.yi.hou@gmail.com>
+;; URL: https://github.com/junyi-hou/agent-shell-tramp
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "29.1") (agent-shell "0.50.1") (acp "0.11.1"))
+
+;; This package is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This package is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+
 ;;; Commentary:
 ;;
 ;; This package provides TRAMP support for `agent-shell`.
